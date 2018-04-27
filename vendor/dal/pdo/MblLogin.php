db8ddb6ec41a2c84c819c93783d10261f79fb7df
kuisatz <kuisatzhaderah@gmail.com>
2018-04-27 03:28:38 +0300

    y

--- vendor/dal/pdo/MblLogin.php
+++ vendor/dal/pdo/MblLogin.php
@@ -2385,7 +2385,7 @@ WHERE cast(getdate() AS date) between cast(dy.Donem1BaslangicTarihi AS date) AND
                
                 $doc = \DOMDocument::loadXML('<Table/>');
                 $doc->formatOutput = true;
-				$root = $doc->documentElement;
+		 $root = $doc->documentElement;
                        
                 foreach ($dataValue as $std) { 
                     if ($std  != null) { 
