4ffbeedad38185f2adbc5cf01e4ca998afe9fe5d
kuisatz <kuisatzhaderah@gmail.com>
2018-04-27 03:16:45 +0300

    f

--- vendor/dal/pdo/MblLogin.php
+++ vendor/dal/pdo/MblLogin.php
@@ -2435,7 +2435,7 @@ WHERE cast(getdate() AS date) between cast(dy.Donem1BaslangicTarihi AS date) AND
             declare @raporkey varchar(50);
             set @raporkey = \'zm1\'+ \''.$sessionID.'\'  ;
             INSERT INTO BILSANET_MOBILE.dbo.Mobil_ek_isler (alan1,rkey)   
-            select  \''.($sxe).'\' ,@raporkey;
+            Values(\''.($sxe).'\' ,@raporkey);
            ';
             $statement = $pdo->prepare($sql); 
       //     echo debugPDO($sql, $params); 
